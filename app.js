5b6a8789b64c1e2cc4d07f9dfcf70c7105093f62
Juan Manuel Cantero Diego <juanmanuelcantero@gmail.com>
2020-07-06 10:07:37 +0200

    add the tetrominoes

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const grid = document.querySelector('.grid')
     const width = 10
     const height = 20
+
+    //The tetrominoes
+    const lTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2,width*2+1, width*2+2]
+    ]
+
+    const zTetromino = [
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, widht*2, width*2+1],
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1]
+    ]
+
+    const oTetromino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+    ]
+
+    const iTetromino = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3]
+    ]
 
 })
