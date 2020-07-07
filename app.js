fe2e497a3dace212e9c596b42c3103e8bc3311a1
Juan Manuel Cantero Diego <juanmanuelcantero@gmail.com>
2020-07-07 20:53:51 +0200

    add functions to move left right and rotate the tetrominoes

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const grid = document.querySelector('.grid')
+    let squares = Array.from(grid.querySelectorAll('div'))
     const width = 10
     const height = 20
+    let currentPosition = 4
 
     //The tetrominoes
     const lTetromino = [
@@ -47,6 +49,59 @@ document.addEventListener('DOMContentLoaded', () => {
     let currentRotation = 0
     let current = theTetrominoes[random][currentRotation]
 
-    
+    //draw the shape
+    function draw() {
+        current.forEach( index => {
+            squares[currentPosition + index].classList.add('block')
+        })
+    }
+
+    //undraw the shape
+    function undraw() {
+        current.forEach( index => {
+            squares[currentPosition + index].classList.remove('block')
+        })
+    }
+
+    //move down the shape
+    function moveDown() {
+        undraw()
+        currentPosition = currentPosition += width
+        draw()
+        freeze()
+    }
+
+    //move left and prevent collisions with shapes moving left
+    function moveRight() {
+        undraw()
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
+        if(!isAtRightEdge) currentPosition += 1
+        if (current.some(index => squares[currentPosition + index].classList.contains('block2'))) {
+            currentPosition -= 1
+        }
+        draw()
+    }
+
+    function moveLeft() {
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+        if(!isAtLeftEdge) currentPosition -= 1
+        if(current.some(index => squares[currentPosition + index].classList.contains('block2'))) {
+            currentPosition += 1
+        }
+        draw()
+    }
+
+    //rotate Tetromino
+    function rotate() {
+        undraw()
+        currentRotation ++
+        if (currentRotation === current.length) {
+            currentRotation = 0
+        }
+        current = theTetrominoes[random][currentRotation]
+    }
+
+
 
 })
